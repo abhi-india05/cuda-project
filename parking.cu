ad52690f39ddae10dc2fdbeba8816f20b22b7941
Abhishek Jayanth Holla <164899777+abhi-india05@users.noreply.github.com>
2026-04-20 22:40:27 +0530

    added gpu vs cpu benchmarking

--- parking.cu
+++ parking.cu
@@ -1,3 +1,4 @@
+
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image.h"
@@ -6,8 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda_runtime.h>
-
-#define MAX_SLOTS 30
+#include <time.h>
 
 typedef struct
 {
@@ -19,6 +19,60 @@ typedef struct
     int occupied;
     float density;
 } SlotResult;
+
+#define MAX_SLOTS 30
+
+
+void rgb_to_gray_cpu(unsigned char *rgb, unsigned char *gray, int w, int h)
+{
+    for (int i = 0; i < w * h; i++)
+    {
+        gray[i] = 0.299f * rgb[3 * i] +
+                  0.587f * rgb[3 * i + 1] +
+                  0.114f * rgb[3 * i + 2];
+    }
+}
+
+void sobel_cpu(unsigned char *gray, unsigned char *edges, int w, int h)
+{
+    for (int y = 1; y < h - 1; y++)
+    {
+        for (int x = 1; x < w - 1; x++)
+        {
+            int Gx = -gray[(y - 1) * w + (x - 1)] + gray[(y - 1) * w + (x + 1)] - 2 * gray[y * w + (x - 1)] + 2 * gray[y * w + (x + 1)] - gray[(y + 1) * w + (x - 1)] + gray[(y + 1) * w + (x + 1)];
+
+            int Gy = -gray[(y - 1) * w + (x - 1)] - 2 * gray[(y - 1) * w + x] - gray[(y - 1) * w + (x + 1)] + gray[(y + 1) * w + (x - 1)] + 2 * gray[(y + 1) * w + x] + gray[(y + 1) * w + (x + 1)];
+
+            int mag = abs(Gx) + abs(Gy);
+            edges[y * w + x] = (mag > 80) ? 255 : 0;
+        }
+    }
+}
+
+void classify_roi_cpu(unsigned char *edges, int w, BBox *boxes,
+                      SlotResult *results, int num_slots, float threshold)
+{
+    for (int i = 0; i < num_slots; i++)
+    {
+        BBox b = boxes[i];
+        int edge_pixels = 0;
+        int total = (b.maxx - b.minx + 1) * (b.maxy - b.miny + 1);
+
+        for (int y = b.miny; y <= b.maxy; y++)
+        {
+            for (int x = b.minx; x <= b.maxx; x++)
+            {
+                if (edges[y * w + x] == 255)
+                    edge_pixels++;
+            }
+        }
+
+        float d = (float)edge_pixels / total;
+        results[i].density = d;
+        results[i].occupied = (d > threshold);
+    }
+}
+
 
 
 __global__ void rgb_to_gray(unsigned char *rgb, unsigned char *gray, int w, int h)
@@ -43,7 +97,7 @@ __global__ void sobel_kernel(unsigned char *gray, unsigned char *edges, int w, i
     int Gy = -gray[(y - 1) * w + (x - 1)] - 2 * gray[(y - 1) * w + x] - gray[(y - 1) * w + (x + 1)] + gray[(y + 1) * w + (x - 1)] + 2 * gray[(y + 1) * w + x] + gray[(y + 1) * w + (x + 1)];
 
     int mag = abs(Gx) + abs(Gy);
-    edges[y * w + x] = (mag > 80) ? 255 : 0; // Thresholding built-in
+    edges[y * w + x] = (mag > 80) ? 255 : 0; 
 }
 
 __global__ void classify_roi_kernel(unsigned char *edges, int w, BBox *boxes, SlotResult *results, int num_slots, float threshold)
@@ -81,6 +135,7 @@ __global__ void classify_roi_kernel(unsigned char *edges, int w, BBox *boxes, Sl
 }
 
 
+
 int get_rois(const char *filename, BBox *boxes, int w, int h)
 {
     if (strstr(filename, "parking.jpg"))
@@ -98,24 +153,30 @@ int get_rois(const char *filename, BBox *boxes, int w, int h)
     }
     else
     {
-        int count = 0;
-        for (int i = 0; i < 3; i++)
-            boxes[count++] = (BBox){96, 54 + i * 45, 168, 98 + i * 45};
-        for (int i = 0; i < 3; i++)
-            boxes[count++] = (BBox){220, 54 + i * 45, 289, 98 + i * 45};
-        for (int i = 0; i < 3; i++)
-            boxes[count++] = (BBox){378, 54 + i * 45, 452, 98 + i * 45};
-        for (int i = 0; i < 3; i++)
-            boxes[count++] = (BBox){500, 54 + i * 45, 572, 98 + i * 45};
+#define IMG_W 626.0f
+#define IMG_H 376.0f
 
-        // Bottom Row (Now at y=320 to 450, much safer for a 500px high image)
+        int count = 0;
+
+#define SX(x) ((int)((x) / IMG_W * w))
+#define SY(y) ((int)((y) / IMG_H * h))
+
+        for (int i = 0; i < 3; i++)
+            boxes[count++] = (BBox){SX(96), SY(54 + i * 45), SX(168), SY(98 + i * 45)};
+        for (int i = 0; i < 3; i++)
+            boxes[count++] = (BBox){SX(220), SY(54 + i * 45), SX(289), SY(98 + i * 45)};
+        for (int i = 0; i < 3; i++)
+            boxes[count++] = (BBox){SX(378), SY(54 + i * 45), SX(452), SY(98 + i * 45)};
+        for (int i = 0; i < 3; i++)
+            boxes[count++] = (BBox){SX(500), SY(54 + i * 45), SX(572), SY(98 + i * 45)};
+
         for (int i = 0; i < 8; i++)
-        {
-            boxes[count++] = (BBox){230 + i * 42, 250, 274 + i * 42, 322};
-        }
+            boxes[count++] = (BBox){SX(230 + i * 42 + 2), SY(250), SX(274 + i * 42), SY(322)};
+
         return count;
     }
 }
+
 
 
 int main(int argc, char **argv)
@@ -124,7 +185,10 @@ int main(int argc, char **argv)
     int w, h, c;
     unsigned char *img = stbi_load(path, &w, &h, &c, 3);
     if (!img)
+    {
+        printf("Can't open image\n");
         return 1;
+    }
 
     BBox h_boxes[MAX_SLOTS];
     int num_slots = get_rois(path, h_boxes, w, h);
@@ -132,6 +196,23 @@ int main(int argc, char **argv)
     unsigned char *d_rgb, *d_gray, *d_edges;
     BBox *d_boxes;
     SlotResult *d_results;
+
+    unsigned char *gray_cpu = (unsigned char *)malloc(w * h);
+    unsigned char *edges_cpu = (unsigned char *)malloc(w * h);
+    SlotResult cpu_results[MAX_SLOTS];
+
+    clock_t cpu_start = clock();
+
+    rgb_to_gray_cpu(img, gray_cpu, w, h);
+    sobel_cpu(gray_cpu, edges_cpu, w, h);
+    classify_roi_cpu(edges_cpu, w, h_boxes, cpu_results, num_slots, 0.35f);
+
+    clock_t cpu_end = clock();
+    double cpu_time = 1000.0 * (cpu_end - cpu_start) / CLOCKS_PER_SEC;
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
 
     cudaMalloc(&d_rgb, w * h * 3);
     cudaMalloc(&d_gray, w * h);
@@ -145,10 +226,36 @@ int main(int argc, char **argv)
     dim3 block(16, 16);
     dim3 grid((w + 15) / 16, (h + 15) / 16);
 
+    cudaEventRecord(start);
     rgb_to_gray<<<grid, block>>>(d_rgb, d_gray, w, h);
+
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    float gpu_time = 0, total_time = 0;
+    cudaEventElapsedTime(&gpu_time, start, stop);
+    total_time += gpu_time;
+    printf("Gray scale time: %f\n", gpu_time);
+
+    cudaEventRecord(start);
     sobel_kernel<<<grid, block>>>(d_gray, d_edges, w, h);
 
-    classify_roi_kernel<<<num_slots, dim3(16, 16)>>>(d_edges, w, d_boxes, d_results, num_slots, 0.4f);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    cudaEventElapsedTime(&gpu_time, start, stop);
+    total_time += gpu_time;
+    printf("Sobel kernel time: %f\n", gpu_time);
+
+    cudaEventRecord(start);
+    classify_roi_kernel<<<num_slots, dim3(16, 16)>>>(d_edges, w, d_boxes, d_results, num_slots, 0.35f);
+
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    cudaEventElapsedTime(&gpu_time, start, stop);
+    total_time += gpu_time;
+    printf("Classification kernel time: %f\n", gpu_time);
 
     SlotResult h_results[MAX_SLOTS];
     cudaMemcpy(h_results, d_results, sizeof(SlotResult) * num_slots, cudaMemcpyDeviceToHost);
@@ -185,8 +292,14 @@ int main(int argc, char **argv)
                 }
             }
         }
+        printf("%d %d %d %d \n", b.minx, b.maxx, b.miny, b.maxy);
         printf("Slot %d: %s (Density: %.2f%%)\n", i, h_results[i].occupied ? "OCCUPIED" : "VACANT", h_results[i].density * 100);
     }
+
+    printf("\n===== PERFORMANCE =====\n");
+    printf("CPU Time: %.2f ms\n", cpu_time);
+    printf("GPU Time: %.2f ms\n", total_time);
+    printf("Speedup: %.2fx\n", cpu_time / total_time);
 
     stbi_write_png("final_result.png", w, h, 3, img, w * 3);
     printf("Saved result to final_result.png\n");
